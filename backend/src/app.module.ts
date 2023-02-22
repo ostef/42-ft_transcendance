4480421ada2d1bf0301a89a5c92e555048720982
glemoine <glemoine@student.42lyon.fr>
2023-02-22 16:47:06 +0100

    docker compose update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import * as process from 'process';
 
 @Module({
-	imports: [AuthModule, UsersModule],
-	controllers: [AppController],
-	providers: [AppService, UsersService],
+  imports: [
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'database',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      //TODO: Change to false in production
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
+im;
+
 // This should be a real class/interface representing a user entity
 export type User = any;
 
