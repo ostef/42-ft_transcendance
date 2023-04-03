f726c53b7f8b0abc4934945945cbd0c955edc554
Stefan <oumansour.stefan@gmail.com>
2023-04-03 15:17:44 +0200

    Update types.ts

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -9,10 +9,14 @@ export interface IChannel
 {
 	id: number;
 	name: string;
+	description: string;
+	ownerId: number;
 }
 
 export interface IUser
 {
 	id: number;
 	username: string;
+	profilePictureURL: string;
+	isModerator: boolean;
 }
