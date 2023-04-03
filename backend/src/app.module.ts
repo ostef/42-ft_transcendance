8f72709c78e57415a825930c67abe397f3af6748
Stefan <oumansour.stefan@gmail.com>
2023-04-03 15:36:54 +0200

    Fixed compile errors

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 import { UsersModule } from './users/users.module';
-
-import { Friendship, User } from './users/user.entity';
-
-import { ChatService } from './chat/chat.service';
 import { ChatModule } from './chat/chat.module';
-import { Conversation, Message } from './chat/chat.entity';
 import { FilesModule } from './files/files.module';
 
 import * as process from 'process';
@@ -28,7 +23,7 @@ import * as process from 'process';
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
       entities: ["users/*.entity.ts", "chat/entities/*.ts" ],
-      
+
       //TODO: Change to false in production
       synchronize: true,
     }),
