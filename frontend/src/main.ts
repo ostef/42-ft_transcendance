e75076378bf6c86df1b5d6ea6c3ee636eb69e4ed
Stefan <47391841+ostef@users.noreply.github.com>
2023-05-31 15:45:36 +0200

    Added axios and iconify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,21 @@ import "./assets/app.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { Icon } from "@iconify/vue";
+
+import axios from "axios";
+import VueAxios from "vue-axios";
+import router from "./router";
 
 import App from "./App.vue";
-import router from "./router";
 
 const app = createApp (App);
 
 app.use (createPinia ());
 app.use (router);
+app.use (VueAxios, axios);
+app.component ("iconify-icon", Icon)
+
+axios.defaults.baseURL = "http://" + location.hostname + ":3000";
 
 app.mount ("#app");
