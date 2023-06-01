532cd8ccd0fbcc24586380ef821e63ee116f6241
Stefan <47391841+ostef@users.noreply.github.com>
2023-06-01 20:52:00 +0200

    Chat messages

---
+++ frontend/src/components/ChatMessage.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+defineProps ({
+    username: String,
+    picture: String,
+    time: String,
+    mine: Boolean,
+    content: String,
+});
+
+</script>
+
+<template>
+    <div class="chat m-4" :class="mine ? 'chat-end' : 'chat-start'">
+        <div class="chat-image avatar">
+            <div class="h-12 w-12 rounded-full overflow-hidden">
+                <img :src="picture" />
+                <!-- <iconify-icon icon="ri:account-circle-line" class="h-12 w-12 rounded-full" /> -->
+            </div>
+        </div>
+
+        <div class="chat-header">
+            {{ username }}
+        </div>
+
+        <div class="chat-bubble" :class="mine ? 'chat-bubble-primary' : 'chat-bubble-secondary'">
+            {{ content }}
+        </div>
+
+        <div class="chat-footer">
+            <time class="text-xs opacity-50">{{ time }}</time>
+        </div>
+    </div>
+</template>
--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -1,5 +1,32 @@
+<script setup lang="ts">
+
+import ChatMessage from "../components/ChatMessage.vue";
+
+</script>
+
 <template>
+    <div class="flex w-full h-3/4">
+        <!-- <ChatDiscussionList /> -->
+        <div class="w-1/4 overflow-y-scroll p-4 bg-base-200">
+        </div>
 
-<button class="btn">Hello Chat!</button>
+        <div class="w-3/4 overflow-y-scroll p-4 bg-base-200">
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 1" :mine=false />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 2" :mine=false />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 3" :mine=true />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 4" :mine=false />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 5" :mine=false />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 6" :mine=true />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 7" :mine=true />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 8" :mine=false />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 9" :mine=false />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 10" :mine=true />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 11" :mine=false />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 12" :mine=true />
+            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 13" :mine=false />
+        </div>
 
+        <div class="w-1/4 overflow-y-scroll p-4 bg-base-200">
+        </div>
+    </div>
 </template>
