f4e7cd5816f3136ea0485f7245b8d3edc12e5a10
Stefan <47391841+ostef@users.noreply.github.com>
2023-06-05 15:50:18 +0200

    Fetch messages from chat store

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -1,32 +1,50 @@
 <script setup lang="ts">
 
+import { reactive, computed } from "vue";
+
 import ChatMessage from "../components/ChatMessage.vue";
+
+import { useChatStore } from "../stores/chat";
+import { useUserStore } from "../stores/user";
+
+const chat = useChatStore ();
+const user = useUserStore ();
 
 </script>
 
 <template>
     <div class="flex w-full h-3/4">
         <!-- <ChatDiscussionList /> -->
-        <div class="w-1/4 overflow-y-auto mx-2 p-4 bg-base-200">
+        <div class="w-1/4 overflow-y-auto mx-2 p-4 rounded-lg bg-base-200">
+            <div v-for="disc of chat.discussions">
+                <div v-if="disc.isDirect">
+                </div>
+                <div v-else>
+                    {{ disc.channel?.name }} <br>
+                    {{ disc.channel?.description }} <br>
+                </div>
+            </div>
         </div>
 
-        <div class="w-3/4 overflow-y-auto mx-2 p-4 bg-base-200">
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 1" :mine=false />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 2" :mine=false />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 3" :mine=true />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 4" :mine=false />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 5" :mine=false />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 6" :mine=true />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 7" :mine=true />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 8" :mine=false />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 9" :mine=false />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 10" :mine=true />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 11" :mine=false />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 12" :mine=true />
-            <ChatMessage username="soumanso" picture="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp" time="12:45" content="Hello Sailor 13" :mine=false />
+        <div class="w-3/4 mx-2 overflow-hidden rounded-lg bg-base-200">
+            <div class="overflow-y-auto p-4 h-5/6">
+                <ChatMessage v-for="msg of chat.messages"
+                    :username="msg.sender.username"
+                    :picture="msg.sender.profilePictureUrl"
+                    time="12:45"
+                    :content="msg.content"
+                    :mine="msg.sender.id == user.user.id"
+                />
+            </div>
+
+            <div class="p-4 h-1/6 mt-4">
+                <input type="text" placeholder="Write something" class="input input-bordered w-4/6" />
+                <button class="btn normal-case mx-4 w-1/6">Send</button>
+            </div>
         </div>
 
-        <div class="w-1/4 overflow-y-auto mx-2 p-4 bg-base-200">
+
+        <div class="w-1/4 overflow-y-auto mx-2 p-4 rounded-lg bg-base-200">
         </div>
     </div>
 </template>
