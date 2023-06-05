40a0e4f800faf15748d9d987d5953f83e4f27205
Gregoire <gregoire007@hotmail.fr>
2023-06-05 16:14:09 +0200

    pull rewrite branch plus begin adding back 42 intra backend connection and frontend profil view

---
+++ backend/src/auth/42.strategy.ts
@@ -0,0 +1,35 @@
+import { Strategy } from 'passport-42';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class FortyTwoStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      clientID: process.env.FORTYTWO_CLIENT_ID,
+      clientSecret: process.env.FORTYTWO_CLIENT_SECRET,
+      callbackURL: process.env.FORTYTWO_CALLBACK_URL,
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(
+    request: any,
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: any,
+  ) {
+    // const user = await this.authService.validateUser(profile);
+    // if (!user) {
+    //   throw new UnauthorizedException();
+    // }
+    const user = {
+      username: profile.username,
+      nickname: profile.username,
+      email: profile.email,
+    };
+    done(null, user);
+  }
+}
