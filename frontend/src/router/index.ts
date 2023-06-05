d6e86b6c43d463da544c647331f420ac3c02f190
Gregoire <gregoire007@hotmail.fr>
2023-06-05 16:35:33 +0200

    Revert "pull rewrite branch plus begin adding back 42 intra backend connection and frontend profil view"
    This reverts commit 40a0e4f800faf15748d9d987d5953f83e4f27205.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
 import ChatView from "../views/Chat.vue";
-import
 
 const router = createRouter ({
     history: createWebHistory (import.meta.env.BASE_URL),
