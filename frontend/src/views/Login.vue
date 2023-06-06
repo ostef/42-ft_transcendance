4d8d2036e4f6b69bd49ee6d04820252847546463
Stefan <47391841+ostef@users.noreply.github.com>
2023-06-06 21:59:31 +0200

    Implemented login form

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,5 +1,43 @@
 <template>
-    <div class="h-full w-full">
-        <button class="btn normal-case">Login</button>
+    <div class="grid max-w-xs">
+        <input class="input input-bordered w-full max-w-xs m-2"
+            type="text" placeholder="Username" v-model="username" />
+        <input class="input input-bordered w-full max-w-xs m-2"
+            type="password" placeholder="Password" v-model="password" />
+        <button class="btn normal-case m-2" @click="login ()">Login</button>
     </div>
 </template>
+
+<script setup lang="ts">
+
+import { ref } from "vue";
+import axios from "axios";
+import router from "@/router";
+
+const username = ref ("");
+const password = ref ("");
+
+async function login ()
+{
+    if (username.value == "" || password.value == "")
+        return;
+
+    try
+    {
+        const res = await axios.post ("auth/login", { username: username.value, password: password.value });
+
+        const token: string = res.data.access_token;
+        localStorage.setItem ("token", token);
+
+        router.replace ("/");
+    }
+    catch (err: any)
+    {
+        if (err.response)
+            console.log ("Could not log in: ", err.response.data);
+        else
+            console.log ("Could not log in");
+    }
+}
+
+</script>
