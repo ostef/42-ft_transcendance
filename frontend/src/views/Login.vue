a5365e0cfdbe7428ee6966ca80420dad4ef1a6d3
Stefan <47391841+ostef@users.noreply.github.com>
2023-06-06 22:53:29 +0200

    Update Login.vue

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="grid max-w-xs">
+    <div class="grid w-full place-content-center">
         <input class="input input-bordered w-full max-w-xs m-2"
             type="text" placeholder="Username" v-model="username" />
         <input class="input input-bordered w-full max-w-xs m-2"
             type="password" placeholder="Password" v-model="password" />
-        <button class="btn normal-case m-2" @click="login ()">Login</button>
+        <button class="btn normal-case m-2 max-w-xs" @click="login ()">Login</button>
     </div>
 </template>
 
