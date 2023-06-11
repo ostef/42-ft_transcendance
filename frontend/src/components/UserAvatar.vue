5a2a8e684bc6014a84e2b058da8730ed8655d43a
Stefan <47391841+ostef@users.noreply.github.com>
2023-06-11 18:35:06 +0200

    User avatar interaction progress

--- frontend/src/components/UserAvatar.vue
+++ frontend/src/components/UserAvatar.vue
@@ -1,26 +1,70 @@
 <script setup lang="ts">
 
+import axios from "axios";
+
 defineProps ({
+    userId: String,
     username: String,
     picture: String,
     online: Boolean,
     left: Boolean,
+    isBlocked: Boolean,
+    isFriend: Boolean,
 });
+
+async function sendFriendRequest (id: string | undefined)
+{
+    if (id == undefined)
+        return;
+
+    await axios.post ("user/friends/add/" + id);
+}
+
+async function removeFriend (id: string | undefined)
+{
+    if (id == undefined)
+        return;
+
+    await axios.put ("user/", { friendsToRemove: [id] });
+}
+
+async function blockUser (id: string | undefined)
+{
+    if (id == undefined)
+        return;
+
+    await axios.put ("user/", { usersToBlock: [id] });
+}
+
+async function unblockUser (id: string | undefined)
+{
+    if (id == undefined)
+        return;
+
+    await axios.put ("user/", { usersToUnblock: [id] });
+}
 
 </script>
 
 <template>
-    <div class="dropdown flex" :class="left ? 'dropdown-left' : 'dropdown-right'" >
+    <div class="tooltip dropdown flex" :class="left ? 'dropdown-left' : 'dropdown-right'" :data-tip="username" >
         <label tabindex="0" class="avatar" :class="online ? 'online' : 'offline'">
             <div class="h-12 w-12 btn btn-circle overflow-hidden">
                 <img :src="picture" />
             </div>
         </label>
         <ul tabindex="0" class="menu menu-compact dropdown-content w-40 m-2 shadow rounded bg-base-300">
-            <li><a>Add Friend</a></li>
-            <li><a>Invite To Play</a></li>
+            <li>
+                <a v-if="!isFriend" @click="sendFriendRequest (userId)">Add Friend</a>
+                <a v-else @click="removeFriend (userId)">Remove Friend</a>
+
+            </li>
+            <li><a :class="isBlocked ? 'btn-disabled' : ''">Invite To Play</a></li>
             <li><a>See User Profile</a></li>
-            <li><a class="bg-accent hover:bg-accent-focus">Block User</a></li>
+            <li>
+                <a v-if="!isBlocked" class="bg-accent hover:bg-accent-focus" @click="blockUser (userId)">Block User</a>
+                <a v-else class="bg-accent hover:bg-accent-focus" @click="unblockUser (userId)">Unblock User</a>
+            </li>
         </ul>
     </div>
 </template>
