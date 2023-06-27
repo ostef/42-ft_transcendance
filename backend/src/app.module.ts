4e2e4a6e8fc5d5fa94f9256abf3c9f0ffbc35ffe
Stefan <47391841+ostef@users.noreply.github.com>
2023-06-27 13:24:48 +0200

    Removed unused app controller and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { APP_GUARD } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
+import { AuthModule } from "./auth/auth.module";
+import { JwtAuthGuard } from "./auth/jwt_auth.guard";
+import { ChatModule } from "./chat/chat.module";
 
 import { UserEntity } from "./users/entities/user.entity";
 import { FriendRequestEntity } from "./users/entities/friend_request.entity";
@@ -12,10 +13,6 @@ import { ChannelEntity } from "./chat/entities/channel.entity";
 import { MessageEntity } from "./chat/entities/message.entity";
 import { ChannelInviteEntity } from "./chat/entities/channel_invite.entity";
 import { PrivateConversationEntity } from "./chat/entities/private_conversation.entity";
-
-import { AuthModule } from "./auth/auth.module";
-import { JwtAuthGuard } from "./auth/jwt_auth.guard";
-import { ChatModule } from "./chat/chat.module";
 
 @Module({
     imports: [
@@ -38,9 +35,7 @@ import { ChatModule } from "./chat/chat.module";
             synchronize: true,
         }),
     ],
-    controllers: [AppController],
     providers: [
-        AppService,
         {
             provide: APP_GUARD,
             useClass: JwtAuthGuard,
