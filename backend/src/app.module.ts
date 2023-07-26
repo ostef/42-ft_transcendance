d541a62a54601574ec66b5b131e96f9ec5598ed9
Gregoire <glemoine@student.42lyon.fr>
2023-07-26 17:19:00 +0200

    add postgres hostname to env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -25,7 +25,7 @@ import { gameHistoryEntity } from "./game/entities/gameHistory.entity";
         ChatModule,
         TypeOrmModule.forRoot ({
             type: 'postgres',
-            host: 'database',
+            host: process.env.POSTGRES_HOST,
             port: 5432,
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
