5086c3e4274fbf376887cada34be04fed950a317
Stefan <47391841+ostef@users.noreply.github.com>
2023-07-26 19:53:12 +0200

    Use daisyui theme colors

--- frontend/src/components/game/WaitingForPlayer.vue
+++ frontend/src/components/game/WaitingForPlayer.vue
@@ -12,10 +12,10 @@ export default {
 
 <template>
     <div class="flex content-center justify-center place-content-center">
-        <div class="flex content-center justify-center bg-gray-600 rounded-lg w-1/4" >
+        <div class="flex content-center justify-center bg-base-200 rounded-lg w-1/4" >
             <span class="label-text text-lg fontwin-bold mr-4 p-2">Waiting for player</span>
             <span class="loading loading-infinity loading-lg"></span>
         </div>
-        <router-link to="/game/0" class="btn bg-gray-600" @click="$emit('disconnect')">Quit</router-link>
+        <router-link to="/game/0" class="btn bg-base-600" @click="$emit('disconnect')">Quit</router-link>
     </div>
 </template>
